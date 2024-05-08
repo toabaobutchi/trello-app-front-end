5ac0c263de5825c257f51533692dc301bdf72568
ngohoaian <ngohoaian01031@gmail.com>
2024-05-08 12:21:54 +0700

    update AccountMenu

--- src/components/Header/partials/UtilitiesMenu/partials/AccountMenu/index.tsx
+++ src/components/Header/partials/UtilitiesMenu/partials/AccountMenu/index.tsx
@@ -1,6 +1,7 @@
 import FixedMenu from '@comps/FixedMenu'
 import MenuGroup from '@comps/MenuGroup'
 import MenuItem from '@comps/MenuItem'
+import './AccountMenu.scss'
 
 function AccountMenu() {
   return (
@@ -16,24 +17,32 @@ function AccountMenu() {
         style={{ top: '3.43rem', right: '0.3%' }}
         width='330px'
       >
-        <MenuGroup title={{ content: 'Account', style: { textTransform: 'capitalize' } }} divisor expandGroup={null}>
-          <MenuItem>Item 1</MenuItem>
-          <MenuItem>Item 2</MenuItem>
-          <MenuItem>Item 3</MenuItem>
+        <MenuGroup title={{ content: 'Account', style: { textTransform: 'uppercase' } }} divisor expandGroup={null}>
+          <MenuItem>
+            <div className='account-info'>
+              <span className='account-info-avatar'>AH</span>
+              <div className='account-info-item'>
+                <p className='account-info-display-name'>Ngô Hoài An</p>
+                <p className='account-info-email'>ngohoaian01031@gmail.com</p>
+              </div>
+            </div>
+          </MenuItem>
+          <MenuItem>Switch accounts</MenuItem>
+          <MenuItem>Manage account</MenuItem>
         </MenuGroup>
         <MenuGroup title={{ content: 'Trello', style: { textTransform: 'uppercase' } }} divisor expandGroup={null}>
-          <MenuItem>Item 1</MenuItem>
-          <MenuItem>Item 2</MenuItem>
-          <MenuItem>Item 3</MenuItem>
-          <MenuItem>Item 2</MenuItem>
-          <MenuItem>Item 3</MenuItem>
+          <MenuItem>Profile and visibility</MenuItem>
+          <MenuItem>Activity</MenuItem>
+          <MenuItem>Card</MenuItem>
+          <MenuItem>Setting</MenuItem>
+          <MenuItem>Theme</MenuItem>
         </MenuGroup>
         <MenuGroup divisor expandGroup={null}>
-          <MenuItem>Item 1</MenuItem>
-          <MenuItem>Item 2</MenuItem>
+          <MenuItem>Help</MenuItem>
+          <MenuItem>Shortcut</MenuItem>
         </MenuGroup>
         <MenuGroup expandGroup={null}>
-          <MenuItem>Item 1</MenuItem>
+          <MenuItem>Log out</MenuItem>
         </MenuGroup>
       </FixedMenu>
     </>
