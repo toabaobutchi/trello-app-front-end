eaa805245f9ee0ebd8631b6bf464783e94c18a22
ngohoaian <ngohoaian01031@gmail.com>
2024-08-21 16:58:29 +0700

    update project

--- src/layouts/SideBar/partials/SidebarAccount/index.tsx
+++ src/layouts/SideBar/partials/SidebarAccount/index.tsx
@@ -3,8 +3,8 @@ import useAccount from '@hooks/useAccount'
 function SidebarAccount() {
   const account = useAccount()
   return (
-    <div className='sidebar-account'>
-      <img className='sidebar-account-avatar' src={account?.avatar} alt='avatar' />
+    <div className='sidebar-account cpointer border border-thin border-light border-hover-primary primary-radius'>
+      <img className='sidebar-account-avatar avatar-img avatar-img__small' src={account?.avatar} alt='avatar' />
       <div className='sidebar-account-info sidebar-text'>
         <p className='sidebar-account-name'>{account?.displayName}</p>
         <p className='sidebar-account-email'>{account?.email}</p>
